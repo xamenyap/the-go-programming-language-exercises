1aa9a46265885ce430942060ab2e8d1107d9bc8a
Xamenyap <jpham2811@gmail.com>
2022-02-07 20:10:28 +0700

    Uupdate 4.12: read/write xkcds json from/to file to prevent calling xkcd APIs multiple times

--- chapter4/4.12/main.go
+++ chapter4/4.12/main.go
@@ -15,23 +15,9 @@ func main() {
 		return
 	}
 
-	var xkcds []xkcd
-	nums := []int{570, 571, 572}
-	const url = "https://xkcd.com/%d/info.0.json"
-
-	for _, n := range nums {
-		resp, err := http.Get(fmt.Sprintf(url, n))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var xkcd xkcd
-		if err := json.NewDecoder(resp.Body).Decode(&xkcd); err != nil {
-			log.Fatal(err)
-		}
-
-		resp.Body.Close()
-		xkcds = append(xkcds, xkcd)
+	xkcds, err := getXkcds()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	search := os.Args[1]
@@ -46,6 +32,72 @@ func main() {
 	for _, xkcd := range found {
 		fmt.Printf("%d\t%s\t%s\t%s\n", xkcd.Num, xkcd.Year, xkcd.Title, xkcd.Img)
 	}
+}
+
+func getXkcds() ([]xkcd, error) {
+	var xkcds []xkcd
+
+	xkcds, err := readXkcds()
+	if err != nil || len(xkcds) == 0 {
+		nums := []int{570, 571, 572}
+		const url = "https://xkcd.com/%d/info.0.json"
+
+		for _, n := range nums {
+			resp, err := http.Get(fmt.Sprintf(url, n))
+			if err != nil {
+				return nil, err
+			}
+
+			var xkcd xkcd
+			if err := json.NewDecoder(resp.Body).Decode(&xkcd); err != nil {
+				return nil, err
+			}
+
+			resp.Body.Close()
+			xkcds = append(xkcds, xkcd)
+		}
+	}
+
+	if err := writeXkcds(xkcds); err != nil {
+		return nil, err
+	}
+
+	return xkcds, nil
+}
+
+func readXkcds() ([]xkcd, error) {
+	var xkcds []xkcd
+	f, err := os.OpenFile("./xkcd.json", os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	if err := json.NewDecoder(f).Decode(&xkcds); err != nil {
+		return nil, err
+	}
+
+	return xkcds, nil
+}
+
+func writeXkcds(xkcds []xkcd) error {
+	marshaledXkcds, err := json.Marshal(xkcds)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile("./xkcd.json", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = f.Write(marshaledXkcds); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type xkcd struct {
