c1e2d016eebe6ad9d0095b2d01df15596e3c0ae5
Xamenyap <jpham2811@gmail.com>
2022-05-15 09:32:31 +0700

    Add solution for 8.3

---
+++ chapter8/8.3/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	if cn, ok := conn.(*net.TCPConn); ok {
+		cn.CloseWrite()
+	} else {
+		conn.Close()
+	}
+
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
