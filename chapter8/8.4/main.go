13a7e77028a94f4b93444bd8ce90e09f2f884388
xamenyap <jpham2811@gmail.com>
2022-10-19 20:58:07 +0800

    Add solution to 8.4 exercise

---
+++ chapter8/8.4/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go func(text string) {
+			echo(c, text, 1*time.Second)
+			wg.Done()
+		}(input.Text())
+	}
+
+	wg.Wait()
+
+	if cn, ok := c.(*net.TCPConn); ok {
+		cn.CloseWrite()
+	} else {
+		c.Close()
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
